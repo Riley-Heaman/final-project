b3e5d538c76f20fa529d7666ab0dc91744b17029
Riley-Heaman <93946712+Riley-Heaman@users.noreply.github.com>
2021-12-14 18:42:03 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+//Riley Heaman
+//CIS 219: Mr. Swinarski
+//Final Project - Happy Birthday? Message & Age
+//**DATE**
+public class Main {
+    /*PSEUDO-CODE
+    -yourBirthday?
+    -today'sDate?
+    -age?
+    -message */
+}
